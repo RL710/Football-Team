bae38ac5c52f0e4013708b19220660ac30648b26
renelv <rene.lv@live.de>
2017-02-10 19:14:43 +0100

    +Scale Banner works. +Switching between content too. -scaling of layout is wrong (tempfix with scaling in newsbutton)

--- app/src/main/java/com/example/android/sveteam/MainActivity.java
+++ app/src/main/java/com/example/android/sveteam/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.sveteam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +14,90 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private void scalePic(){
+
+    private void scaleLayout() {
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
         int w, h;
         w = linearLayout.getWidth();
-        h = 2;
+        h = linearLayout.getHeight();
+        LinearLayout ll = (LinearLayout) findViewById(R.id.ll_content);
+        ll.setLayoutParams(new LinearLayout.LayoutParams(w, h));
+    }
+
+    private void scalePic() {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
+        int w, h;
+        w = linearLayout.getWidth();
+        h = w/2;
         ImageView imageView = (ImageView) findViewById(R.id.lable_pic);
-        imageView.setMaxWidth(w);
-        imageView.setMinimumWidth(w);
-        imageView.setMaxHeight(h);
-        imageView.setMinimumHeight(h);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(w, h));
+    }
+
+
+
+    private void changeTextViewContent(String str) {
+        TextView textView = (TextView) findViewById(R.id.textview_news);
+        textView.setText(str);
+    }
+
+    public void changeToNews(View view) {
+        scalePic();
+        scaleLayout();
+        makeNewsVisible(true);
+        makeInfoVisible(false);
+        makeTableVisible(false);
 
     }
+
+    public void changeToTable(View view) {
+        makeTableVisible(true);
+        makeInfoVisible(false);
+        makeNewsVisible(false);
+    }
+
+    public void changeToInfo(View view) {
+        makeNewsVisible(false);
+        makeInfoVisible(true);
+        makeTableVisible(false);
+    }
+
+    //visibility of content
+
+    private void makeNewsVisible(boolean boo) {
+        if (boo) {
+            TextView textView = (TextView) findViewById(R.id.textview_news);
+            textView.setVisibility(View.VISIBLE);
+        } else {
+            TextView textView = (TextView) findViewById(R.id.textview_news);
+            textView.setVisibility(View.GONE);
+
+        }
+
+    }
+
+    private void makeInfoVisible(boolean boo) {
+        if (boo) {
+            TextView textView = (TextView) findViewById(R.id.textview_info);
+            textView.setVisibility(View.VISIBLE);
+        } else {
+            TextView textView = (TextView) findViewById(R.id.textview_info);
+            textView.setVisibility(View.GONE);
+
+        }
+
+    }
+
+    private void makeTableVisible(boolean boo) {
+        if (boo) {
+            ImageView img = (ImageView) findViewById(R.id.content_table);
+            img.setVisibility(View.VISIBLE);
+        } else {
+            ImageView img = (ImageView) findViewById(R.id.content_table);
+            img.setVisibility(View.GONE);
+
+        }
+
+    }
+
+
 }
